44f0493c32d766c10a5d3c50b36550cfb0b57808
Marcela Celani <104652600+marcela-celani@users.noreply.github.com>
2022-05-04 21:53:52 -0300

    Add files via upload

---
+++ programamozinho.py
@@ -0,0 +1,15 @@
+operacao = "+" #digite um operador entre +, -, * ou /
+numero1 = 2 #digite um numero
+numero2 = 3 #digite um numero
+
+if operacao == "+":
+	print(numero1+numero2)
+
+elif operacao == "-":
+	print(numero1-numero2)
+
+elif operacao == "*":
+	print(numero1*numero2)
+
+elif operacao == "/":
+	print(numero1/numero2)
